3cfab14948eb759bddadb3ec510eb9b5c05ad111
MalvaCerealis <107715807+MalvaCerealis@users.noreply.github.com>
2026-01-17 11:27:55 -0500

    website up to date

---
+++ load.js
@@ -0,0 +1,5 @@
+$(function(){
+      $("#header").load("https://angeldrool.neocities.org/header.html"); 
+      $("#navigation").load("https://angeldrool.neocities.org/navigation.html");
+      $("#sidebar").load("https://angeldrool.neocities.org/sidebar.html"); 
+});
